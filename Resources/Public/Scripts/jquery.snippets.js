4b353dc0137671e01cd8e89b04b1a311624a3118
khuppenbauer <k.huppenbauer@web.de>
2014-07-21 07:20:14 +0200

    [TASK] adjust pagination

--- Resources/Public/Scripts/jquery.snippets.js
+++ Resources/Public/Scripts/jquery.snippets.js
@@ -12,7 +12,7 @@ $(".jq-filter-reset").click(function() {
 });
 
 //set filter from links with data attribute
-$('a[data-category], a[data-author], a[data-tags]').click(function(){
+$('a[data-category], a[data-author], a[data-tags], a[data-currentPage]').click(function(){
     var attributes = $(this).data();
     $.each(attributes, function(k,v) {
         if (k != 'currentpage') {
