0e28d218aaec6f5c7f1f57234487e7e8eccd046e
khuppenbauer <k.huppenbauer@web.de>
2015-05-03 10:54:38 +0200

    [TASK] optimize packagist request

--- Classes/Flowpack/Snippets/Controller/PackagistController.php
+++ Classes/Flowpack/Snippets/Controller/PackagistController.php
@@ -31,6 +31,11 @@ class PackagistController extends ActionController {
 	protected $postRepository;
 
 	/**
+	 * @var string
+	 */
+	protected $packagistUrl = 'https://packagist.org';
+
+	/**
 	 * @return void
 	 */
 	public function initializeObject() {
@@ -52,24 +57,50 @@ class PackagistController extends ActionController {
 	 */
 	public function searchAction($start, $length, $search = array()) {
 		$page = ($start/$length)+1;
-		$uri = 'https://packagist.org/search.json?type=' . $search['value'] . '&page=' . $page;
-		$response = $this->browser->request($uri);
-		$content = $response->getContent();
-		$contentArray = json_decode($content, TRUE);
-		foreach ($contentArray['results'] as $key => $item) {
-			$data['data'][$key]['title'] = $item['name'];
-			$data['data'][$key]['description'] = $item['description'];
-			$data['data'][$key]['downloads'] = '<i class="fa fa-download"></i>' . $item['downloads'];
-			$data['data'][$key]['favers'] = '<i class="fa fa-star"></i>' . $item['favers'];
-			$disabled = $this->postRepository->findOneByPackage($item['name']) !== NULL ? ' disabled' : '';
-			$item['repository'] = str_replace(array('git@github.com:'), 'https://github.com/', $item['repository']);
-			$item['repository'] = str_replace(array('git://'), 'https://', $item['repository']);
-			$data['data'][$key]['button'] = '<a href="#" data-title="' . $item['name'] . '" data-description="' . $item['description'] . '" data-url="' . $item['repository'] . '" class="jq-take btn btn-default' . $disabled . '"><i class="fa fa-check"></i></a>';
+		$filters['page'] = $page;
+		$filters['type'] = $search['value'];
+		$contentArray = $this->searchPackagist($filters);
+		if ($contentArray['total'] === 0) {
+			$data['data'] = array();
+		} else {
+			foreach ($contentArray['results'] as $key => $item) {
+				$data['data'][$key]['title'] = '<a href="' . $this->packagistUrl . '/packages/' . $item['name'] . '" target="_blank">' . $item['name'] . '</a>';
+				$data['data'][$key]['description'] = $item['description'];
+				$data['data'][$key]['downloads'] = '<i class="fa fa-download"></i>' . $item['downloads'];
+				$data['data'][$key]['favers'] = '<i class="fa fa-star"></i>' . $item['favers'];
+				$disabled = $this->postRepository->findOneByPackage($item['name']) !== NULL ? ' disabled' : '';
+				$item['repository'] = str_replace(array('git@github.com:'), 'https://github.com/', $item['repository']);
+				$item['repository'] = str_replace(array('git://'), 'https://', $item['repository']);
+				$data['data'][$key]['button'] = '<a href="#" data-title="' . $item['name'] . '" data-description="' . $item['description'] . '" data-url="' . $item['repository'] . '" class="jq-take btn btn-default' . $disabled . '"><i class="fa fa-check"></i></a>';
+			}
 		}
 		$data['recordsTotal'] = $contentArray['total'];
 		$data['recordsFiltered'] = $contentArray['total'];
-		$data['uri'] = $uri;
+		$data['uri'] = $contentArray['url'];
 		return json_encode($data);
+	}
+
+	/**
+	 * @param $filters
+	 * @return array
+	 */
+	protected function searchPackagist($filters) {
+		$url = $this->packagistUrl . '/search.json?' . http_build_query($filters);
+		$response = $this->browser->request($url);
+		$content = $response->getContent();
+		$contentArray = json_decode($content, TRUE);
+		$contentArray['url'] = $url;
+		if ($contentArray['total'] === 0) {
+			if (isset($filters['type'])) {
+				$filters['q'] = $filters['type'];
+				unset($filters['type']);
+				return $this->searchPackagist($filters);
+			} else {
+				return $contentArray;
+			}
+		} else {
+			return $contentArray;
+		}
 	}
 
 
